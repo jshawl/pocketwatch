c15a44c6a27bf0c3578ba9f7065cecf24c31972d
Jesse Shawl <jesse@jshawl.com>
2013-11-09 01:03:57 +0000

    added template to default script

--- server/index.php
+++ server/index.php
@@ -19,9 +19,9 @@
   $('.reading-list').readingList({
       access_token: '<?php echo $_GET['access_token'];?>',
       limit:10,
-      filter: 'favorite'
+      filter: 'favorite',
+      template: '<h1><a href="{{resolved_url}}">{{resolved_title}}</a></h1>{{excerpt}}'
    });
-
   &lt;/script>
 </pre>
 <?php } ?>
